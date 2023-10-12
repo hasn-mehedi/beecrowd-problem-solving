51970978eb38c5bd0cc591c99b6cff2d7ec6ad1c
Mehedi Hasan <144303039+hasn-mehedi@users.noreply.github.com>
2023-10-12 11:28:49 +0600

    Add files via upload

---
+++ Bee1016.java
@@ -0,0 +1,20 @@
+package beginner;
+
+import java.util.Scanner;
+
+public class Bee1016 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		Scanner scanner = new Scanner(System.in);
+		
+		int distance = scanner.nextInt();
+		
+		int time = distance * 2;
+		
+		System.out.printf("%d minutos\n", time);
+
+	}
+
+}
